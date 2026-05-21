4c59a1e0cc449c1398f828923fa8eb6c89a87f79
lexamania <torbanowmain@gmail.com>
2026-05-21 22:44:32 +0300

    add docker configs

--- scripts/installers/install_pack_development.sh
+++ scripts/installers/install_pack_development.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-../base/_pacman.sh npm nodejs dotnet-sdk docker docker-compose
+../base/_pacman.sh npm nodejs dotnet-sdk
 ../base/_yay.sh visual-studio-code-bin
+
+../custom/setup_docker.sh
