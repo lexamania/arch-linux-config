e5e03bfeb40657bd05643385352a3114aeb52ccd
lexamania <torbanowmain@gmail.com>
2026-05-26 21:55:35 +0300

    add aspnet-runtime

--- scripts/installers/install_pack_development.sh
+++ scripts/installers/install_pack_development.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-../base/_pacman.sh npm nodejs dotnet-sdk
+../base/_pacman.sh npm nodejs dotnet-sdk aspnet-runtime
 ../base/_yay.sh visual-studio-code-bin
 
 ../custom/setup_docker.sh
